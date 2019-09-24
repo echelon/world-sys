2f55502bc5574d03b3213c788d40ac5ef6550ec5
Brandon Thomas <echelon@gmail.com>
2019-09-23 23:40:10 -0400

    Wrappers for WORLD vocoder

---
+++ generate_bindgen.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if ! which bindgen > /dev/null; then
+    echo "ERROR: Please install 'bindgen' using cargo:"
+    echo "    cargo install bindgen"
+    echo "See https://github.com/servo/rust-bindgen for more information."
+    exit 1
+fi
+
+bindgen_options=""
+#header="./World/src/world/harvest.h"
+header="/usr/local/include/world/harvest.h"
+
+cmd="bindgen ${bindgen_options} ${header} --output src/bindgen.rs"
+echo ${cmd}
+${cmd}
+
