bd0d5c2866cfb5291b00d69adcf1e745e540d9f1
Brandon Thomas <echelon@gmail.com>
2019-10-02 00:55:21 -0400

    compiles, but will be incorrect

--- src/core.rs
+++ src/core.rs
@@ -3,6 +3,9 @@
 //! These are things that are built into WORLD or provided by
 //! the 'pyworld' bindings.
 //!
+//! See:
+//! https://github.com/JeremyCCHsu/Python-Wrapper-for-World-Vocoder/blob/master/pyworld/pyworld.pyx
+//!
 
 use std::os::raw::c_int;
 
@@ -534,9 +537,44 @@ pub struct SynthesizeResult {
   output_waveform: Vec<f64>,
 }
 
-pub fn synthesize() -> SynthesizeResult {
+pub fn synthesize(f0: Vec<f64>,
+                  spectrogram: Vec<f64>, // 2D
+                  aperiodicity: Vec<f64>, // 2D
+                  fs: i32,
+                  frame_period: Option<f64>) -> SynthesizeResult {
+
+  // Defaults
+  let frame_period = frame_period.unwrap_or(5.0f64);
+
+  /*
+  cdef int f0_length = <int>len(f0)
+  y_length = int(f0_length * frame_period * fs / 1000)
+  cdef int fft_size = (<int>spectrogram.shape[1] - 1)*2
+  cdef np.ndarray[double, ndim=1, mode="c"] y = \
+  np.zeros(y_length, dtype=np.dtype('float64'))
+  */
+  let fft_size = (spectrogram.len() - 1) * 2; // TODO: Incorrect wrt multidimensional
+
+  // FIXME -- Not sure this is correct allocation!
+  // But I'm not sure these are the correct lengths...
+  let mut wav: Vec<f64> = vec![0.0f64; f0.len()];
+
+  unsafe {
+    Synthesis(
+      f0.as_ptr(),
+      f0.len() as c_int,
+      spectrogram.as_ptr() as *const _, // TODO: Does this work? I invented this! ----- ??????
+      aperiodicity.as_ptr() as *const _, // TODO: Does this work? I invented this! ----- ??????
+      fft_size as c_int,
+      frame_period,
+      fs as c_int,
+      wav.len() as c_int,
+      wav.as_mut_ptr() as *mut _,
+    );
+  }
+
   SynthesizeResult {
-    output_waveform: vec![],
+    output_waveform: wav,
   }
 }
 
