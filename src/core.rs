7c5d2ac9d6ba6feca37bcc618eb972e5ae4a64d5
Brandon Thomas <echelon@gmail.com>
2019-10-01 21:23:44 -0400

    more outline

--- src/core.rs
+++ src/core.rs
@@ -9,6 +9,8 @@ use std::os::raw::c_int;
 // Constants
 use super::world_kCeilF0;
 use super::world_kFloorF0;
+use super::world_kFloorF0D4C; // TODO: Used?
+use super::world_kThreshold;
 
 // CheapTrick
 use super::CheapTrick;
@@ -16,6 +18,11 @@ use super::CheapTrickOption;
 use super::GetF0FloorForCheapTrick;
 use super::GetFFTSizeForCheapTrick;
 use super::InitializeCheapTrickOption;
+
+// D4C
+use super::D4C;
+use super::D4COption;
+use super::InitializeD4COption;
 
 // Harvest
 use super::GetSamplesForHarvest;
@@ -75,7 +82,7 @@ extern "C" {
 */
 
 pub struct CheapTrickResult {
-  spectrogram: Vec<f64>
+  pub spectrogram: Vec<f64>
 }
 
 /**
@@ -124,6 +131,98 @@ pub fn cheaptrick(wav: Vec<f64>,
 
   CheapTrickResult {
     spectrogram,
+  }
+}
+
+/*
+
+def d4c(*args, **kwargs): # real signature unknown
+    """
+    D4C aperiodicity estimation algorithm.
+
+        Parameters
+        ----------
+        x : ndarray
+            Input waveform signal.
+        f0 : ndarray
+            Input F0 contour.
+        temporal_positions : ndarray
+            Temporal positions of each frame.
+        fs : int
+            Sample rate of input signal in Hz.
+        q1 : float
+            Spectral recovery parameter.
+            Default: -0.15 (this value was tuned and normally does not need adjustment)
+        threshold : float
+            Threshold for aperiodicity-based voiced/unvoiced decision, in range 0 to 1.
+            If a value of 0 is used, voiced frames will be kept voiced. If a value > 0 is
+            used some voiced frames can be considered unvoiced by setting their aperiodicity
+            to 1 (thus synthesizing them with white noise). Using `threshold=0` will result
+            in the behavior of older versions of D4C. The current default of 0.85 is meant
+            to be used in combination with the Harvest F0 estimator, which was designed to have
+            a high voiced/unvoiced threshold (i.e. most frames will be considered voiced).
+            Default: 0.85
+        fft_size : int, None
+            FFT size to be used. When `None` (default) is used, the FFT size is computed
+            automatically as a function of the given input sample rate and the default F0 floor.
+            When `fft_size` is specified, it should match the FFT size used to compute
+            the spectral envelope (i.e. `fft_size=2*(sp.shape[1] - 1)`) in order to get the
+            desired results when resynthesizing.
+            Default: None
+
+        Returns
+        -------
+        aperiodicity : ndarray
+            Aperiodicity (envelope, linear magnitude relative to spectral envelope).
+    """
+    pass
+
+pub struct D4COption {
+    pub threshold: f64,
+}
+
+    pub fn D4C(
+        x: *const f64,
+        x_length: ::std::os::raw::c_int,
+        fs: ::std::os::raw::c_int,
+        temporal_positions: *const f64,
+        f0: *const f64,
+        f0_length: ::std::os::raw::c_int,
+        fft_size: ::std::os::raw::c_int,
+        option: *const D4COption,
+        aperiodicity: *mut *mut f64,
+    );
+*/
+
+pub struct D4CResult {
+  pub aperiodicity: Vec<f64>,
+}
+
+/**
+ * D4C: aperiodicity estimation algorithm.
+ */
+pub fn d4c(wav: Vec<f64>,
+           f0: Vec<f64>,
+           temporal_postions: Vec<f64>,
+           fs: i32,
+           q1: Option<f64>,
+           threshold: Option<f64>,
+           fft_size: Option<i32>) -> D4CResult {
+
+  // Pyworld Defaults
+  let threshold = threshold.unwrap_or(world_kThreshold); // default: 0.85
+
+  let mut option = D4COption::default();
+  option.threshold = threshold;
+
+  // TODO
+  /*unsafe {
+    D4C(
+    );
+  }*/
+
+  D4CResult {
+    aperiodicity: vec![],
   }
 }
 
