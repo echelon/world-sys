34eb062a05dc6c9861dc890a442494f4f8ef27c6
Brandon Thomas <echelon@gmail.com>
2019-10-01 21:04:20 -0400

    outline algorithm

--- src/extras.rs
+++ src/extras.rs
@@ -13,6 +13,37 @@ use std::os::raw::c_int;
 
 use core::*;
 
+/*
+TODO: ALGORITHM TO GET VOICE TRANSFORMATION:
+
+- [mono]
+- [librosa.resample (downsample)]
+- core convert()
+    - wav_padding()
+        - [maths]
+        - [np.pad]
+    - world_decompose()
+        - pyworld.harvest
+        - pyworld.cheaptrick
+        - pyworld.d4c
+    - world_encode_spectral_envelope()
+        - pyworld.code_spectral_envelope
+    - [matrix transpose]
+    - pitch_conversion()
+        - [maths]
+    - [maths : coded_sp_norm]
+    - << evaluate ML model >>
+    - [maths : coded_sp_converted]
+    - [matrix transpose]
+    - [np.ascontiguousarray]
+    - world_decode_spectral_envelop
+        - pyworld.get_cheaptrick_fft_size
+        - pyworld.decode_spectral_envelope
+    - world_speech_synthesis
+        - pyworld.synthesize
+    - [librosa.resample (upsample)]
+    - [wav things]
+*/
 
 pub struct DecomposeResult {
 }
