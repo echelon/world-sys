1b6465c06f9debec6fcf82bf672bf57effc6ddd7
Brandon Thomas <echelon@gmail.com>
2019-10-07 03:22:42 -0400

    D4C is now stack overflowing instead of segfaulting! :)

--- src/core/d4c.rs
+++ src/core/d4c.rs
@@ -1,6 +1,8 @@
 
 use std::os::raw::c_int;
+use std::mem;
 use std::ptr;
+use std::alloc::{alloc, dealloc, Layout};
 
 use super::cheaptrick::get_cheaptrick_fft_size;
 
@@ -115,14 +117,137 @@ pub fn d4c(wav: Vec<f64>,
   //let size = f0.len() * (fft_size / 2 + 1) as usize;
   //let mut aperiodicity: Vec<f64> = vec![0.0f64; size];
   // https://github.com/neithanmo/csound-rs/blob/46b50fa94ebb869d051b7d7f74555c76ecd4cbe9/src/callbacks.rs
-  let mut ptr = ::std::ptr::null_mut();
+
+  /*
+  static void InitializeAperiodicity(int f0_length, int fft_size, double **aperiodicity) {
+      for (int i = 0; i < f0_length; ++i)
+        for (int j = 0; j < fft_size / 2 + 1; ++j)
+          aperiodicity[i][j] = 1.0 - world::kMySafeGuardMinimum;
+  }
+
+ ========================================
+
+ Unrelated, but:
+
+ extern "C" {
+    pub fn OGR_G_CreateFromWkt(
+        arg1: *mut *mut libc::c_char,
+        arg2: OGRSpatialReferenceH,
+        arg3: *mut OGRGeometryH,
+    ) -> OGRErr::Type;
+ }
+
+     pub fn from_wkt(wkt: &str) -> Result<Geometry> {
+        let c_wkt = CString::new(wkt)?;
+        let mut c_wkt_ptr = c_wkt.into_raw();
+        let mut c_geom = null_mut();
+        let rv = unsafe { gdal_sys::OGR_G_CreateFromWkt(&mut c_wkt_ptr, null_mut(), &mut c_geom) };
+
+======================================
+
+And,
+
+    pub(crate) fn get_raw_channel_ptr(
+        &self,
+        name: &str,
+        ptr: *mut *mut f64,
+        channel_type: c_int,
+    ) -> c_int {
+        let cname = match CString::new(name) {
+            Ok(c) => c,
+            Err(_) => return -1,
+        };
+        unsafe {
+            csound_sys::csoundGetChannelPtr(self.engine.csound, ptr, cname.as_ptr(), channel_type)
+        }
+    }
+
+               fn get_input_channel(&'a self, name: &str, _: $t) -> Result<ChannelPtr<'a, $t, Writable>, Status> {
+
+                let mut ptr = ptr::null_mut() as *mut f64;
+                let ptr = &mut ptr as *mut *mut _;
+                let len;
+                let bits;
+
+                match $e {
+                    ChannelInternalType::AUDIO => {
+                        len = self.get_ksmps() as usize;
+                        bits = (csound_sys::CSOUND_AUDIO_CHANNEL | csound_sys::CSOUND_INPUT_CHANNEL) as c_int;
+                    },
+                    ChannelInternalType::CONTROL => {
+                        len = 1;
+                        bits = (csound_sys::CSOUND_CONTROL_CHANNEL | csound_sys::CSOUND_INPUT_CHANNEL) as c_int;
+                    },
+                    ChannelInternalType::STR => {
+                        len = self.get_channel_data_size(name) as usize;
+                        bits = (csound_sys::CSOUND_STRING_CHANNEL | csound_sys::CSOUND_INPUT_CHANNEL) as c_int;
+                    },
+                }
+
+                unsafe {
+                    let result = Status::from(self.get_raw_channel_ptr(name, ptr, bits));
+ptr
+
+use std::alloc::{alloc, dealloc, Layout};
+
+unsafe {
+    let layout = Layout::new::<u16>();
+    let ptr = alloc(layout);
+
+    *(ptr as *mut u16) = 42;
+    assert_eq!(*(ptr as *mut u16), 42);
+
+    dealloc(ptr, layout);
+}
+
+Hi y'all, my apologies if this is the wrong channel. I'm using bindgen
+to wrap a C++ library, but I'm having the hardest time allocating and passing
+a two-dimensional array of f64. The C++ function takes a `double **aperiodicity`,
+which is expected to be allocated by the caller. Bindgen wrapped this as
+`aperiodicity: *mut *mut f64,`. How do I allocate this and pass it from Rust?
+
+  */
+
+  /*let mut ptr = ::std::ptr::null_mut();
   let aperiodicity: *mut *mut f64 = &mut ptr as *mut *mut _;
 
+  let mut ptr = ptr::null_mut() as *mut f64;
+  let aperiodicity= &mut ptr as *mut *mut _;
+
+  let mut ptr = [[50.0f64; 1000]; 1000];*/
+
+  // TODO: This is how we do it!
+  let mut ptr : Vec<*mut f64> = Vec::new();
+  for i in 0 .. 100_000 {
+    ptr.push(ptr::null_mut());
+  }
+
+  //let aperiodicity= ptr as *mut _;
+  //let aperiodicity= ptr.as_mut_ptr() as *mut _;
+
   unsafe {
+    //let aperiodicity = mem::transmute::<[[f64;1000]; 1000], *mut *mut f64>(ptr);
+
+
+    //let layout = Layout::array::<f64>(1000usize).unwrap();
+    //let mut ptr = alloc(layout);
+    //let aperiodicity= &mut ptr as *mut *mut _;
+
     /*
         D4C(&x[0], x_length, fs, &temporal_positions[0],
         &f0[0], f0_length, fft_size0, &option,
         cpp_aperiodicity)
+
+C++
++ (gdb) print aperiodicity
+$15 = (double **) 0x7fffff858dc8
+
+Rust
++ (gdb) print aperiodicity
+$18 = (f64 **) 0x7fffff858dc8
++ (gdb) print ptr
+value requires 8000000 bytes, which is more than max-value-size
+
     */
     D4C(
       wav.as_ptr(),
